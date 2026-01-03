7c70e40a6f46ad5d4aafc512e675cb3bf993b218
0x7b <the0x7b@proton.me>
2026-01-04 01:47:41 +0200

    fix: correct cookie diff logic

--- core/diff.go
+++ core/diff.go
@@ -65,7 +65,7 @@ func Diffs(bl []types.BlinkResponse, fc types.FlagCondition) []types.TestResult 
 		if baseline.Timings.FullRtt*2 < r.Timings.FullRtt {
 			res.Diffs = append(res.Diffs, diffLine(types.DiffRTT, strconv.FormatInt(int64(baseline.Timings.FullRtt), 10), strconv.FormatInt(int64(r.Timings.FullRtt), 10), fc))
 		}
-
+		DiffCookie(baseline.Cookies, r.Cookies, types.FlagCondition{})
 		if len(baseline.Cookies) != len(r.Cookies) {
 			res.Diffs = append(res.Diffs, diffLine(types.DiffCookies, strconv.Itoa(len(baseline.Cookies)), strconv.Itoa(len(r.Cookies)), fc))
 		}
@@ -114,5 +114,142 @@ func shortHash(hash string) string {
 		return hash[:10]
 	} else {
 		return "EMPTY_HASH"
+	}
+}
+
+func DiffCookie(bl []*http.Cookie, res []*http.Cookie, fc types.FlagCondition) []types.Diff {
+	var diffs []types.Diff
+
+	blm := CookieMap(bl)
+	resm := CookieMap(res)
+
+	for n := range blm {
+		_, ok := resm[n]
+		if !ok {
+			diffs = append(diffs, diffLine(types.DiffCookies, "deleted", "", fc))
+			continue
+		}
+		if blm[n].Raw == resm[n].Raw {
+			continue
+		}
+		bc := blm[n]
+		rc := resm[n]
+
+		if bc.Value != rc.Value {
+			diffs = append(diffs,
+				diffLine(types.DiffCookies, bc.Value, rc.Value, fc))
+		}
+
+		if bc.Quoted != rc.Quoted {
+			diffs = append(diffs,
+				diffLine(
+					types.DiffCookies,
+					strconv.FormatBool(bc.Quoted),
+					strconv.FormatBool(rc.Quoted),
+					fc,
+				))
+		}
+
+		if bc.Path != rc.Path {
+			diffs = append(diffs,
+				diffLine(types.DiffCookies, bc.Path, rc.Path, fc))
+		}
+
+		if bc.Domain != rc.Domain {
+			diffs = append(diffs,
+				diffLine(types.DiffCookies, bc.Domain, rc.Domain, fc))
+		}
+
+		if !bc.Expires.Equal(rc.Expires) {
+			diffs = append(diffs,
+				diffLine(
+					types.DiffCookies,
+					bc.Expires.String(),
+					rc.Expires.String(),
+					fc,
+				))
+		}
+
+		if bc.MaxAge != rc.MaxAge {
+			diffs = append(diffs,
+				diffLine(
+					types.DiffCookies,
+					strconv.Itoa(bc.MaxAge),
+					strconv.Itoa(rc.MaxAge),
+					fc,
+				))
+		}
+
+		if bc.Secure != rc.Secure {
+			diffs = append(diffs,
+				diffLine(
+					types.DiffCookies,
+					strconv.FormatBool(bc.Secure),
+					strconv.FormatBool(rc.Secure),
+					fc,
+				))
+		}
+
+		if bc.HttpOnly != rc.HttpOnly {
+			diffs = append(diffs,
+				diffLine(
+					types.DiffCookies,
+					strconv.FormatBool(bc.HttpOnly),
+					strconv.FormatBool(rc.HttpOnly),
+					fc,
+				))
+		}
+
+		if bc.SameSite != rc.SameSite {
+			diffs = append(diffs,
+				diffLine(
+					types.DiffCookies,
+					sameSiteToString(bc.SameSite),
+					sameSiteToString(rc.SameSite),
+					fc,
+				))
+		}
+
+		if bc.Partitioned != rc.Partitioned {
+			diffs = append(diffs,
+				diffLine(
+					types.DiffCookies,
+					strconv.FormatBool(bc.Partitioned),
+					strconv.FormatBool(rc.Partitioned),
+					fc,
+				))
+		}
+
+	}
+	for n := range resm {
+		_, ok := blm[n]
+		if !ok {
+			diffs = append(diffs, diffLine(types.DiffCookies, "added", "", fc))
+			continue
+		}
+	}
+	return diffs
+}
+
+func CookieMap(cookiem []*http.Cookie) map[string]*http.Cookie {
+	m := make(map[string]*http.Cookie, len(cookiem))
+	for _, c := range cookiem {
+		m[c.Name] = c
+	}
+
+	return m
+}
+func sameSiteToString(s http.SameSite) string {
+	switch s {
+	case http.SameSiteDefaultMode:
+		return "Default"
+	case http.SameSiteLaxMode:
+		return "Lax"
+	case http.SameSiteStrictMode:
+		return "Strict"
+	case http.SameSiteNoneMode:
+		return "None"
+	default:
+		return "Unknown"
 	}
 }
